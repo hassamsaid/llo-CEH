0cc3a7784d65e9535dc32fd5e3c21da293444ed4
sundade-lab <sundade2421@gmail.com>
2026-08-12 13:56:49 -0500

    correjido

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Bienvenido al equipo de: Carlos Bedolla");
-         System.out.println("Emilio")
+        System.out.println("Emilio González");
         System.out.println("Hassam Said");
     }
 }
