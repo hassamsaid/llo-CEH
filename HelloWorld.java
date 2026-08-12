9eee9ed0fe216bdf61f1317ad20f6c996b135ea3
ELCARLANGAS45 <carlos.bedolla@anahuac.mx>
2026-08-12 13:16:25 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al equipo de: ");
+        System.out.println("Bienvenido al equipo de: Carlos Bedolla");
     }
 }
