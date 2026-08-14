eda62cf3615fb3cead7d634cc299d895dce8a260
ELCARLANGAS45 <carlos.bedolla@anahuac.mx>
2026-08-14 00:46:40 -0500

    Lógica de multiplicación y división terminada, me avisan si surge algun problema o quieren que modifique algo.

---
+++ calculator/multiplicacion.java
@@ -0,0 +1,8 @@
+public class multiplicacion {
+    public double hacermultiplicacion(double numero1, double numero2) {
+        return numero1 * numero2;
+    }
+    public double hacerdivision(double numero1, double numero2) {
+        return numero1 / numero2;
+    }
+}        
